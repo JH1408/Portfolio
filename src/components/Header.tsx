ec00665acf14763fc09c6a323df295892bad3fe9
Josy <josy_hartig@yahoo.de>
2025-04-22 20:33:47 +0700

    fix styling issues

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -87,7 +87,7 @@ const Header: React.FC<{ activeSection?: string }> = ({ activeSection }) => {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
-                transition={{ duration: 0.3 }}
+                transition={{ duration: 0.4 }}
                 className="fixed left-0 right-0 top-0 p-5 pt-[50px] bg-mediumdarkblue text-white"
               >
                 <ul className="grid gap-2">
@@ -99,7 +99,7 @@ const Header: React.FC<{ activeSection?: string }> = ({ activeSection }) => {
                         type: "spring",
                         stiffness: 100,
                         damping: 20,
-                        delay: 0.1 + index / 10,
+                        delay: index / 10,
                       }}
                       key={id}
                       className={`w-full p-[0.08rem] ${scrollStyleLink}`}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero: React.FC = () => {
     <section
       id="hero"
       style={{ backgroundImage: `url(${hero})` }}
-      className="bg-cover bg-center bg-no-repeat bg-fixed bg-darkblue blur-[0.5px]-z-5 text-white w-full h-screen pt-0 mt-0 relative block"
+      className="bg-cover bg-center bg-no-repeat bg-fixed bg-mediumdarkblue blur-[0.5px]-z-5 text-white w-full h-screen pt-0 mt-0 relative block"
     >
       <div className="font-bold font-sans uppercase absolute top-[45%] w-full animate-fadeInUp">
         <h1 className="sm:text-6xl text-5xl whitespace-nowrap leading-normal tracking-[0.1rem]">
